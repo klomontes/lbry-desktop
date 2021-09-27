9ca653c3526215ef4dc3e488b7f13787b86687a3
infinite-persistence <inf.persistence@gmail.com>
2021-09-27 11:44:44 +0800

    comment.list: fix typos and renamed variables
    - Switch from 'author' to 'creator' to disambiguate between comment author and content author. For comment author, we'll use 'commenter' from now on.
    - Corrected 'commenterClaimId' to 'creatorClaimId' (just a typo, no functional change).

--- ui/redux/reducers/comments.js
+++ ui/redux/reducers/comments.js
@@ -245,7 +245,7 @@ export default handleActions(
         claimId,
         uri,
         disabled,
-        commenterClaimId,
+        creatorClaimId,
       } = action.data;
 
       const commentById = Object.assign({}, state.commentById);
@@ -261,8 +261,8 @@ export default handleActions(
       const isLoadingByParentId = Object.assign({}, state.isLoadingByParentId);
       const settingsByChannelId = Object.assign({}, state.settingsByChannelId);
 
-      settingsByChannelId[commenterClaimId] = {
-        ...(settingsByChannelId[commenterClaimId] || {}),
+      settingsByChannelId[creatorClaimId] = {
+        ...(settingsByChannelId[creatorClaimId] || {}),
         comments_enabled: !disabled,
       };
 
